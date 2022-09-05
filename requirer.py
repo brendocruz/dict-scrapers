d2f81e7d4769a07682da39ebcf45f492ae7ae343
Brendo Cruz <43189865+brendocruz@users.noreply.github.com>
2022-09-05 20:22:05 -0300

    Add Scraper class

--- requirer.py
+++ requirer.py
@@ -1,9 +1,20 @@
 import requests
 
-requests_session = requests.Session()
+class Scraper():
+    def __init__(self, base_url=None, url_id=None):
+        self.base_url     = '' if base_url is None else base_url
+        self.url          = '' if url_id is None else url_id 
+        self.scraped_page = None
+        self.timeout      = 4
 
-def load_page(url, timeout=4):
-    res = requests_session.get(url, headers={
+    def _full_url(self):
+        return f'{self.base_url}{self.url}'
+
+    def _load_page(self):
+        self.scraped_page = requests.Session().get(self._full_url(), headers={
             "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-    }, timeout=timeout)
-    return res
+        }, timeout=self.timeout)
+
+    def extract(self):
+        self._load_page()
+        return self.scraped_page.text
