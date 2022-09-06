237764b84395764408502d559ffa5da2b4657e0f
Brendo Cruz <43189865+brendocruz@users.noreply.github.com>
2022-09-05 21:02:48 -0300

    Modift Scraper's load_page function

--- scrapers/requirer.py
+++ scrapers/requirer.py
@@ -1,17 +1,13 @@
 import requests
 
 class Scraper():
-    def __init__(self, base_url=None, url_id=None):
-        self.base_url     = '' if base_url is None else base_url
-        self.url          = '' if url_id is None else url_id 
+    def __init__(self, url):
+        self.url = url
         self.scraped_page = None
         self.timeout      = 4
 
-    def _full_url(self):
-        return f'{self.base_url}{self.url}'
-
     def _load_page(self):
-        self.scraped_page = requests.Session().get(self._full_url(), headers={
+        self.scraped_page = requests.Session().get(self.url, headers={
             "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
         }, timeout=self.timeout)
 
