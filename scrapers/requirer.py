06b58272d20a0e41ea13a458f69eb111f4c20370
Brendo Cruz <43189865+brendocruz@users.noreply.github.com>
2022-09-06 17:27:30 -0300

    Add to Scraper class

--- scrapers/requirer.py
+++ scrapers/requirer.py
@@ -1,16 +1,25 @@
 import requests
 
 class Scraper():
-    def __init__(self, url):
-        self.url = url
-        self.scraped_page = None
-        self.timeout      = 4
+    def __init__(self, url: str):
+        self.url: str     = url
+        self.timeout: int = 4
 
-    def _load_page(self):
-        self.scraped_page = requests.Session().get(self.url, headers={
+    def load(self):
+        self.scraped_page = requests.Session().get(self.full_url(), headers={
             "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
         }, timeout=self.timeout)
 
+    def raw(self):
+        return self.scraped_page
+
     def extract(self):
-        self._load_page()
-        return self.scraped_page.text
+        self.load()
+        return self.raw().text
+
+    def full_url(self) -> str:
+        return self.url
+
+scraper = Scraper('http://www.google.com')
+scraper.load()
+print(scraper.extract())
